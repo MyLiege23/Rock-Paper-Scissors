e9cac13a28ac92509494f857c5ab000dddad0295
MyLiege23 <hsabel@outlook.com>
2022-11-10 15:32:31 -0700

    Complete end-of-game scripts
    includes a count to 5 games, while removing player options and addind a
     "play again" button. Resets score and restores player-options.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,156 @@
 //Rock Paper Scizzors exercise!
 
+//Third Solution ====================================
+
+let rpsOptions = ["rock", "paper", "scissors"];
+const outcomes = {
+  playerWins: 1,
+  tie: 0,
+  computerWins: -1,
+};
+
+function getComputerChoice() {
+  const choice = rpsOptions[~~(Math.random() * rpsOptions.length)];
+  return choice;
+}
+
+/*
+function getPlayerChoice() {
+  return prompt("Rock, paper, or scissors?").toLowerCase();
+}
+*/
+const gameStatus = document.querySelector(".status-text");
+const explanation = document.querySelector(".explanation-text");
+const playerOptionsMenu = document.querySelector(".player-options");
+let playerOptions = Array.from(document.querySelectorAll(".player-option"));
+
+const retryButton = document.createElement("button");
+retryButton.setAttribute("class", "retry-button");
+retryButton.setAttribute("type", "button");
+retryButton.textContent = "Play again?";
+
+let playerScore = 0;
+let playerScoreDisplay = document.querySelector(".player-score");
+playerScoreDisplay.textContent = `${playerScore}`;
+
+let computerScore = 0;
+let computerScoreDisplay = document.querySelector(".computer-score");
+computerScoreDisplay.textContent = `${computerScore}`;
+
+playerOptions.forEach((optionElement) => {
+  optionElement.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    const playerChoice = optionElement.id;
+    const result = rpsWhoWon(computerChoice, playerChoice);
+    if (result == outcomes.playerWins) {
+      gameStatus.textContent = "You won!";
+      explanation.textContent = `${playerChoice} beats ${computerChoice}`;
+      playerScore++;
+      playerScoreDisplay.textContent = `${playerScore}`;
+    } else if (result == outcomes.computerWins) {
+      gameStatus.textContent = "You lost!";
+      explanation.textContent = `${computerChoice} beats ${playerChoice}`;
+      computerScore++;
+      computerScoreDisplay.textContent = `${computerScore}`;
+    } else {
+      gameStatus.textContent = "Tie!";
+      explanation.textContent = `you both chose the same option`;
+    }
+    gameOverAlert();
+  });
+});
+
+let gameOverAlert = function () {
+  if (computerScore == 5 || playerScore == 5) {
+    playerScore == 5
+      ? ((gameStatus.textContent = "You Made It!"),
+        (explanation.textContent = "You were the first to 5"))
+      : ((gameStatus.textContent = "Tough luck!"),
+        (explanation.textContent = "The computer made it to 5 first"));
+    while (playerOptionsMenu.firstChild) {
+      playerOptionsMenu.removeChild(playerOptionsMenu.firstChild);
+    }
+    playerOptionsMenu.appendChild(retryButton);
+  }
+};
+
+retryButton.addEventListener("click", () => {
+  playerScore = 0;
+  playerScoreDisplay.textContent = `${playerScore}`;
+  computerScore = 0;
+  computerScoreDisplay.textContent = `${computerScore}`;
+  playerOptionsMenu.removeChild(retryButton);
+  playerOptions.forEach((optionElement) => {
+    playerOptionsMenu.appendChild(optionElement);
+  });
+});
+
+function rpsWhoWon(computerChoice, playerChoice) {
+  if (computerChoice === playerChoice) {
+    return outcomes.tie;
+  } else if (computerChoice === "rock") {
+    return playerChoice === "paper"
+      ? outcomes.playerWins
+      : outcomes.computerWins;
+  } else if (computerChoice === "paper") {
+    return playerChoice === "scissors"
+      ? outcomes.playerWins
+      : outcomes.computerWins;
+  } else if (computerChoice === "scissors") {
+    return playerChoice === "rock"
+      ? outcomes.playerWins
+      : outcomes.computerWins;
+  }
+}
+
+/* this is unneeded for the UI version of RPS
+function playRPSOutOf(n) {
+  let computerScore = 0;
+  let playerScore = 0;
+  while (computerScore + playerScore < n) {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    console.log("Player: " + playerChoice);
+    console.log("Computer: " + computerChoice);
+    let score = rpsWhoWon(computerChoice, playerChoice);
+    switch (score) {
+      case outcomes.computerWins:
+        console.log("Computer won!");
+        computerScore++;
+        break;
+      case outcomes.playerWins:
+        console.log("Player won!");
+        playerScore++;
+        break;
+      case outcomes.tie:
+        console.log("Tie!");
+        break;
+    }
+  }
+  let gameScore = `Player Score: ${playerScore}; 
+    \nComputer Score: ${computerScore}; 
+    \nGame Score: ${gameScore}.`;
+  return gameScore;
+}
+*/
+
+/*
+
+I overdid the ternary...
+
+function rpsWhoWon(computerChoice, playerChoice) {
+  return (computerChoice === playerChoice ? outcomes.tie
+  : playerChoice === "paper" && computerChoice === "rock" ? outcomes.playerWins
+  : playerChoice === "scissors" && computerChoice === "paper" ? outcomes.playerWins
+  : playerChoice === "rock" && computerChoice === "scissors" ? outcomes.playerWins
+  : playerChoice === "paper" && computerChoice === "scissors" ? outcomes.computerWins
+  : playerChoice === "rock" && computerChoice === "paper" ? outcomes.computerWins
+  : outcomes.computerWins);
+}
+*/
+
+//Previous attempts: first solution====================================
+
 /*
 function playRPS () {
   let player = prompt("Rock, paper, or scissors?").toLowerCase();
@@ -60,6 +211,7 @@ function playRPS(#):
   */
 
 //Second solution below===========================================
+
 /*
 let rpsOptions = ["rock", "paper", "scissors"];
 
@@ -124,84 +276,5 @@ function playRPSOutOf(n) {
     "Player Score: " + playerScore + " \nComputer Score: " + computerScore;
   console.log(gameScore);
   return gameScore;
-}
-*/
-
-//Third Solution Below ====================================
-
-let rpsOptions = ["rock", "paper", "scissors"];
-const outcomes = {
-  playerWins: 1,
-  tie: 0,
-  computerWins: -1,
-};
-
-function getComputerChoice() {
-  return rpsOptions[~~(Math.random() * rpsOptions.length)];
-}
-
-function getPlayerChoice() {
-  return prompt("Rock, paper, or scissors?").toLowerCase();
-}
-
-function rpsWhoWon(computerChoice, playerChoice) {
-  if (computerChoice === playerChoice) {
-    return outcomes.tie;
-  } else if (computerChoice === "rock") {
-    return playerChoice === "paper"
-      ? outcomes.playerWins
-      : outcomes.computerWins;
-  } else if (computerChoice === "paper") {
-    return playerChoice === "scissors"
-      ? outcomes.playerWins
-      : outcomes.computerWins;
-  } else if (computerChoice === "scissors") {
-    return playerChoice === "rock"
-      ? outcomes.playerWins
-      : outcomes.computerWins;
-  }
-}
-
-function playRPSOutOf(n) {
-  let computerScore = 0;
-  let playerScore = 0;
-  while (computerScore + playerScore < n) {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-    console.log("Player: " + playerChoice);
-    console.log("Computer: " + computerChoice);
-    let score = rpsWhoWon(computerChoice, playerChoice);
-    switch (score) {
-      case outcomes.computerWins:
-        console.log("Computer won!");
-        computerScore++;
-        break;
-      case outcomes.playerWins:
-        console.log("Player won!");
-        playerScore++;
-        break;
-      case outcomes.tie:
-        console.log("Tie!");
-        break;
-    }
-  }
-  let gameScore = `Player Score: ${playerScore}; 
-    \nComputer Score: ${computerScore}; 
-    \nGame Score: ${gameScore}.`;
-  return gameScore;
-}
-
-/*
-
-I overdid the ternary...
-
-function rpsWhoWon(computerChoice, playerChoice) {
-  return (computerChoice === playerChoice ? outcomes.tie
-  : playerChoice === "paper" && computerChoice === "rock" ? outcomes.playerWins
-  : playerChoice === "scissors" && computerChoice === "paper" ? outcomes.playerWins
-  : playerChoice === "rock" && computerChoice === "scissors" ? outcomes.playerWins
-  : playerChoice === "paper" && computerChoice === "scissors" ? outcomes.computerWins
-  : playerChoice === "rock" && computerChoice === "paper" ? outcomes.computerWins
-  : outcomes.computerWins);
 }
 */
